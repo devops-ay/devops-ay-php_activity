abd86d8132e35c6018e2b7288d668cf92c9cba17
name <email@email.net>
2018-10-11 19:54:44 -0400

    changed header in index/index.php again (attempt 2)

--- index/index.php
+++ index/index.php
@@ -12,7 +12,7 @@
 	<div id="big_wrapper">
 
 
-	<header id="header">Ali's VCDEP Application</header>
+	<header id="header">Ali's VCDEP</header>
 	<!-- Displaying the navigation bar -->
 	<nav id="navigation">
 		<ul>Home</ul>
